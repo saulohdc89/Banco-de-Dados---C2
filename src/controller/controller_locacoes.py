92d880f439a7c49b655f09a4b5bdde310c0eb3a4
labdatabase <saulohdc89@yahoo.co.jp>
2022-10-18 18:44:47 -0300

    quase

--- src/controller/controller_locacoes.py
+++ src/controller/controller_locacoes.py
@@ -24,21 +24,22 @@ class Controller_Locacoes:
         data_hoje = date.today()
         # Lista os automoveis existentes para inserir na locacao
         self.listar_clientes(oracle, need_connect=True)
-        cpf = str(input("Digite o número do CPF do Cliente: "))
-        cliente = self.valida_cliente(oracle, cpf)
+        n_cpf = str(input("Digite o número do CPF do Cliente: "))
+        cliente = self.valida_cliente(oracle, n_cpf)
         if cliente == None:
             return None
 
-        # Lista os modelos existentes para inserir no item de pedido
+        # Lista os modelos existentes para inserir no locacao
         self.listar_modelos(oracle, need_connect=True)
-        codigo_modelo = str(input("Digite o código dos modelos: "))
-        modelos = self.valida_modelo(oracle, codigo_modelo)
+        novo_modelo = str(input("Digite modelo: "))
+        modelos = self.valida_modelo(oracle, novo_modelo)
         if modelos == None:
             return None
 
+        #Lista os modelos existentes para inserir no locacao
         self.listar_marcas(oracle, need_connect=True)
-        codigo_modelo = str(input("Digite a  "))
-        marcas = self.valida_marca(oracle, codigo_modelo)
+        novo_marcas = str(input("Digite a marca  "))
+        marcas = self.valida_marca(oracle, novo_marcas)
         if marcas == None:
             return None
 
@@ -58,24 +59,23 @@ class Controller_Locacoes:
         output_value = cursor.var(int)
 
         # Cria um dicionário para mapear as variáveis de entrada e saída
-        data = dict(codigo=output_value, data_devolucao = idata_devolucao, , codigo_pedido=int(pedido.get_codigo_pedido()), codigo_produto=int(produto.get_codigo()))
+        data = dict(data_devolucao = idata_devolucao, cpf=cliente.get_cpf(),Placa =automoveis.get_Placa() ,nome_modelo = modelos.get_modelo(),nome_marca = marcas.get_marcas(),data_locacao = data_hoje)
         # Executa o bloco PL/SQL anônimo para inserção do novo item de pedido e recuperação da chave primária criada pela sequence
 
-        #Locacaoes datava_devolucao, cpf, Placa, nome_modelo, nome_marca, data_locacao
+        #Locacaoes data_devolucao, cpf, Placa, nome_modelo, nome_marca, data_locacao
         cursor.execute("""
         begin
-            :data_devolucao := LOCACOES_SEQ.NEXTVAL;
-            insert into locacacoes values(data_devolucao:cpf,:nome_modelo,:nome_marca :data_locacao);
+            :cpf := LOCACOES_SEQ.NEXTVAL;
+            insert into locacoes values(data_devolucao:cpf,:nome_modelo,:nome_marca :data_locacao);
         end;
         """, data)
-        # Recupera o código do novo item de pedido
-        codigo_devolucao = output_value.getvalue()
+       
         # Persiste (confirma) as alterações
         oracle.conn.commit()
         # Recupera os dados do novo item de pedido criado transformando em um DataFrame
-        df_locacao = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")
+        df_locacao = oracle.sqlToDataFrame(f"select data_devolucao, cpf, Placa, nome_modelo, nome_marca, data_locacao from locacoes where cpf = {n_cpf}")
         # Cria um novo objeto Item de Pedido
-        nova_locacao = Locacoes(df_item_pedido.data_devolucao.values[0], df_item_pedido.quantidade.values[0], df_item_pedido.valor_unitario.values[0], pedido, produto)
+        nova_locacao = Locacoes(df_locacao.data_devolucao.values[0],cliente,automoveis,marcas,df_locacao.data_locacao.values[0])
         # Exibe os atributos do novo Item de Pedido
         print(nova_locacao.to_string())
         # Retorna o objeto novo_item_pedido para utilização posterior, caso necessário
@@ -87,42 +87,55 @@ class Controller_Locacoes:
         oracle.connect()
 
         # Solicita ao usuário o código do item de pedido a ser alterado
-        codigo_item_pedido = int(input("Código do Item de Pedido que irá alterar: "))        
+        codigo_item_pedido = input("Digite o cpf: ")        
 
         # Verifica se o item de pedido existe na base de dados
-        if not self.verifica_existencia_item_pedido(oracle, codigo_item_pedido):
+        if not self.verifica_existencia_locacoes(oracle, codigo_item_pedido):
+
+            data_hoje = date.today()
 
             # Lista os pedido existentes para inserir no item de pedido
-            self.listar_pedidos(oracle, need_connect=True)
-            codigo_pedido = str(input("Digite o número do Pedido: "))
-            pedido = self.valida_pedido(oracle, codigo_pedido)
-            if pedido == None:
+            self.listar_clientes(oracle, need_connect=True)
+            cpf = str(input("Digite o número do CPF do Cliente: "))
+            cliente = self.valida_cliente(oracle, cpf)
+            if cliente == None:
                 return None
 
             # Lista os produtos existentes para inserir no item de pedido
-            self.listar_produtos(oracle, need_connect=True)
-            codigo_produto = str(input("Digite o código do Produto: "))
-            produto = self.valida_produto(oracle, codigo_produto)
-            if produto == None:
+            self.listar_modelos(oracle, need_connect=True)
+            novo_modelo = str(input("Digite o modelo "))
+            modelos = self.valida_modelo(oracle, novo_modelo)
+            if modelos == None:
                 return None
 
-            # Solicita a quantidade de itens do pedido para o produto selecionado
-            quantidade = float(input(f"Informe a quantidade de itens do produto {produto.get_descricao()}: "))
-            # Solicita o valor unitário do produto selecionado
-            valor_unitario = float(input(f"Informe o valor unitário do produto {produto.get_descricao()}: "))
+
+            self.listar_marcas(oracle, need_connect=True)
+            novo_marcas = str(input("Digite a marca  "))
+            marcas = self.valida_marca(oracle, novo_marcas)
+            if marcas == None:
+                return None
+            
+            self.listar_automoveis(oracle, need_connect=True)
+            codigo_automoveis = str(input("Digite a placa do automovel: "))
+            automoveis = self.valida_automoveis(oracle, codigo_automoveis)
+            if automoveis == None:
+                return None
+
+            idata_devolucao = input("Data de devolucao:")
+            data_hoje = date.today()
 
             # Atualiza o item de pedido existente
-            oracle.write(f"update itens_pedido set quantidade = {quantidade}, valor_unitario = {valor_unitario}, codigo_pedido = {pedido.get_codigo_pedido()}, codigo_produto = {produto.get_codigo()} where codigo_item_pedido = {codigo_item_pedido}")
+            oracle.write(f"update locacoes set data_locacao = {data_hoje}, data_devolucao = {idata_devolucao}, where cpf = {cpf}")
             # Recupera os dados do novo item de pedido criado transformando em um DataFrame
-            df_item_pedido = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")
+            df_locacao = oracle.sqlToDataFrame(f"select data_devolucao, cpf, Placa, nome_modelo, nome_marca, data_locacao from locacoes where data_devolucao = {cpf}")
             # Cria um novo objeto Item de Pedido
-            item_pedido_atualizado = ItemPedido(df_item_pedido.codigo_item_pedido.values[0], df_item_pedido.quantidade.values[0], df_item_pedido.valor_unitario.values[0], pedido, produto)
+            nova_locacao = Locacoes(df_locacao.data_devolucao.values[0],cliente.automoveis,marcas,df_locacao.data_locacao.values[0])
             # Exibe os atributos do item de pedido
-            print(item_pedido_atualizado.to_string())
+            print(nova_locacao.to_string())
             # Retorna o objeto pedido_atualizado para utilização posterior, caso necessário
-            return item_pedido_atualizado
+            return nova_locacao
         else:
-            print(f"O código {codigo_item_pedido} não existe.")
+            print(f"O {cpf} não existe.")
             return None
 
     def excluir_locacoes(self):
@@ -131,39 +144,40 @@ class Controller_Locacoes:
         oracle.connect()
 
         # Solicita ao usuário o código do item de pedido a ser alterado
-        codigo_item_pedido = int(input("Código do Item de Pedido que irá excluir: "))        
+        cpf = str(input("Digite o número do CPF do Cliente: "))        
 
         # Verifica se o item de pedido existe na base de dados
-        if not self.verifica_existencia_item_pedido(oracle, codigo_item_pedido):            
+        if not self.verifica_existencia_locacao(oracle, cpf):            
             # Recupera os dados do novo item de pedido criado transformando em um DataFrame
-            df_item_pedido = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")
-            pedido = self.valida_pedido(oracle, df_item_pedido.codigo_pedido.values[0])
-            produto = self.valida_produto(oracle, df_item_pedido.codigo_produto.values[0])
+            df_locacao = oracle.sqlToDataFrame(f"select data_devolucao, cpf, Placa, nome_modelo, nome_marca, data_locacao from locacoes where cpf = {cpf}")
+            modelos = self.valida_modelo(oracle, df_locacao.nome_modelos.value[0])
+            marcas = self.valida_marca(oracle, df_locacao.nome_marca.value[0]))
+            automoveis = self.valida_automoveis(oracle, df_locacao.Placa.value[0])
             
-            opcao_excluir = input(f"Tem certeza que deseja excluir o item de pedido {codigo_item_pedido} [S ou N]: ")
+            
+            opcao_excluir = input(f"Tem certeza que deseja excluir locacao CFP {cpf} [S ou N]: ")
             if opcao_excluir.lower() == "s":
                 # Revome o produto da tabela
-                oracle.write(f"delete from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")                
+                oracle.write(f"delete from locacao where cpf = {cpf}")                
                 # Cria um novo objeto Item de Pedido para informar que foi removido
-                item_pedido_excluido = ItemPedido(df_item_pedido.codigo_item_pedido.values[0], df_item_pedido.quantidade.values[0], df_item_pedido.valor_unitario.values[0], pedido, produto)
+                item_pedido_excluido = Locacoes(df_locacao.data_devolucao.values(0), df_locacao.Placa.values(0),df_locacao.nome_modelo.values(0),df_locacao.nome_marca.values(0),df_locacao.data_locacao.values(0))
                 # Exibe os atributos do produto excluído
                 print("Item do Pedido Removido com Sucesso!")
                 print(item_pedido_excluido.to_string())
         else:
             print(f"O código {codigo_item_pedido} não existe.")
 
-    def verifica_locacoes(self, oracle:OracleQueries, codigo:int=None) -> bool:
+    def verifica_existencia_locacoes(self, oracle:OracleQueries, cpf) -> bool:
         # Recupera os dados do novo pedido criado transformando em um DataFrame
-        df_pedido = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo}")
+        df_pedido = oracle.sqlToDataFrame(f"select data_devolucao, cpf, Placa, nome_modelo, nome_marca, data_locacao from locacoes where cpf = {cpf}")
         return df_pedido.empty
 
-    def listar_locacoes(self, oracle:OracleQueries, need_connect:bool=False):
+    def listar_clientes(self, oracle:OracleQueries, need_connect:bool=False):
         query = """
-                select l.data_devolucao
-                    , l.cpf as cliente
-                    , l.nome as cliente
-                    , nvl(f.nome_fantasia, f.razao_social) as empresa
-                    , i.codigo_item_pedido as item_pedido
+                select dl.cpf
+                    , c.cpf as cliente
+                    , c.nome as cliente
+                    , c.endereco as cliente
                     , prd.descricao_produto as produto
                     , i.quantidade
                     , i.valor_unitario
@@ -183,7 +197,7 @@ class Controller_Locacoes:
             oracle.connect()
         print(oracle.sqlToDataFrame(query))
 
-    def listar_produtos(self, oracle:OracleQueries, need_connect:bool=False):
+    def listar_modelos(self, oracle:OracleQueries, need_connect:bool=False):
         query = """
                 select prd.codigo_produto
                     , prd.descricao_produto 
