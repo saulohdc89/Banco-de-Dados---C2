a34de2444e02d2c640ebb3d889c78c8de8ac96ac
labdatabase <saulohdc89@yahoo.co.jp>
2022-10-20 20:17:06 -0300

    menu

--- src/utils/config.py
+++ src/utils/config.py
@@ -7,21 +7,20 @@ MENU_PRINCIPAL = """Menu Principal
 """
 
 MENU_RELATORIOS = """Relatórios
-1 - Relatório de Fornecedores
-2 - Relatório de Pedidos
-3 - Relatório de Produtos
-4 - Relatório de Clientes
-5 - Relatório de Fornecedores
-6 - Relatório de Itens de Pedidos
+1 - Relatório de Cliente
+2 - Relatório de Marcas
+3 - Relatório de Modelos
+4 - Relatório de Automoveis
+5 - Relatório de Locacoes
 0 - Sair
 """
 
 MENU_ENTIDADES = """Entidades
-1 - PRODUTOS
-2 - CLIENTES
-3 - FORNECEDORES
-4 - PEDIDOS
-5 - ITENS DE PEDIDOS
+1 - CLIENTES
+2 - MARCAS
+3 - MODELOS
+4 - AUTOMOVEIS
+5 - LOCACOES
 """
 
 # Consulta de contagem de registros por tabela
